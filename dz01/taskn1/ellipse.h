ededffd6d3a5c1e7aacf63f31e063adbc7ac80cf
lifelessgarmentss <167255405+lifelessgarmentss@users.noreply.github.com>
2025-03-10 11:16:43 +0300

    Update ellipse.h

--- dz01/taskn1/ellipse.h
+++ dz01/taskn1/ellipse.h
@@ -1,1 +1,26 @@
+#pragma once
 
+class Ellipse {
+public:
+    double centerX, centerY, semiMajor, semiMinor;
+    bool isXAxis;
+
+public:
+    Ellipse(double centerX = 0, double centerY = 0, double semiMajor = 0, double semiMinor = 0, bool isXAxis = true);
+    Ellipse(const Ellipse &other);
+    ~Ellipse();
+    void adjustAxes(double &semiMajor, double &semiMinor);
+    double computeHyperparameter();
+    void setParameters(double centerX, double centerY, double semiMajor, double semiMinor, bool isXAxis);
+    void getParameters(double &centerX, double &centerY, double &semiMajor, double &semiMinor, bool &isXAxis);
+    void displayPoints();
+    void displayFoci();
+    void computeChordLength();
+    void computeEccentricity();
+    void showData();
+    void checkPointPosition(double x, double y);
+    double calculatePerimeter();
+    double calculateArea();
+    double findCoordinate(double value, bool isXGiven);
+};
+  
